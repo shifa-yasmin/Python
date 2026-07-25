bf0e70e50818beb787dc1f775d29ac2b97fcef71
shifa <shifayasmin23@gmail.com>
2026-07-25 14:01:35 +0530

    large number

--- hello.py
+++ hello.py
@@ -12,8 +12,19 @@
 #     print("odd")
 
 
-age=3
-if age>=18:
-    print("eligible for vote")
+# age=3
+# if age>=18:
+#     print("eligible for vote")
+# else:
+#     print("not eligible")
+
+
+a=10000
+b=200
+c=100
+if a>b and a>c:
+    print("a large")
+elif b>a and b>c:
+    print("b is large")
 else:
-    print("not eligible")
+    print("c is large")
