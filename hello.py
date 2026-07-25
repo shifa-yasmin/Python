d5c7905e2ef9d53fd94788e6d10c3ebd3af2df7e
shifa <shifayasmin23@gmail.com>
2026-07-25 13:57:33 +0530

    vote

--- hello.py
+++ hello.py
@@ -5,8 +5,15 @@
 #    print("negative")
 
 
-num=5
-if num%2==0:
-    print("even")
+# num=5
+# if num%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+age=3
+if age>=18:
+    print("eligible for vote")
 else:
-    print("odd")
+    print("not eligible")
