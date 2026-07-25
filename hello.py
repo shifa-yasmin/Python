7261da338ef449368ad76ab52d16d21bb03bee4b
shifa <shifayasmin23@gmail.com>
2026-07-25 13:55:10 +0530

    python even or odd

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
-num=-1
-if num>0:
-   print("possitive")
+# num=-1
+# if num>0:
+#    print("possitive")
+# else:
+#    print("negative")
+
+
+num=5
+if num%2==0:
+    print("even")
 else:
-   print("negative")
+    print("odd")
