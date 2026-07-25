2a019d95e22dc92bb60cd40054f7b601ec6eb9f0
shifa <shifayasmin23@gmail.com>
2026-07-25 14:05:57 +0530

    mark grade

--- hello.py
+++ hello.py
@@ -19,12 +19,28 @@
 #     print("not eligible")
 
 
-a=10000
-b=200
-c=100
-if a>b and a>c:
-    print("a large")
-elif b>a and b>c:
-    print("b is large")
+# a=10000
+# b=200
+# c=100
+# if a>b and a>c:
+#     print("a large")
+# elif b>a and b>c:
+#     print("b is large")
+# else:
+#     print("c is large")
+
+
+# mark=18
+# if mark<18:
+#     print("fail")
+# else:
+#     print("pass")
+
+
+mark=39
+if mark>30:
+    print("A grade")
+elif mark>20:
+    print("B grade")
 else:
-    print("c is large")
+    print("D grade")
